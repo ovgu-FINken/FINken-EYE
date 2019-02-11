17179301fab24d74d82d582630bd845ecd0d6bb2
microwav3s <microwav3s@googlemail.com>
2019-02-11 01:13:10 +0100

    Add built SVO and vikit libraries

---
+++ jevois/devel/share/gennodejs/ros/svo_msgs/msg/_index.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let Info = require('./Info.js');
+let NbvTrajectory = require('./NbvTrajectory.js');
+let DenseInput = require('./DenseInput.js');
+let Feature = require('./Feature.js');
+
+module.exports = {
+  Info: Info,
+  NbvTrajectory: NbvTrajectory,
+  DenseInput: DenseInput,
+  Feature: Feature,
+};
